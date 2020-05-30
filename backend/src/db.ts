4e655f2fcefadf90df66fde2bf765c22695f5eeb
Khaled Osman <khaled.osmaan@gmail.com>
2020-05-30 09:12:38 +0200

    chore: fix type issue

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from 'sequelize'
 
-export const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+export const sequelize = new Sequelize('' + process.env.PGDATABASE, '' + process.env.PGUSER, '' + process.env.PGPASSWORD, {
   host: process.env.PGHOST,
   dialect: 'postgres'
 })
 
-export async function initDb (): Sequelize {
+export async function initDb(): Promise<Sequelize> {
   await sequelize.authenticate()
   await sequelize.sync({ force: process.env.NODE_ENV !== 'production' })
   return sequelize
